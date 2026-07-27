80daebaa748981e008970b862bb15b2972151310
chenyu951216-afk <chenyu951216@gmail.com>
2026-07-27 17:37:34 +0800

    Delete app/indicators directory

--- app/indicators/vwap.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-
-def vwap(high: pd.Series, low: pd.Series, close: pd.Series, volume: pd.Series) -> pd.Series:
-    typical = (high.astype(float) + low.astype(float) + close.astype(float)) / 3
-    denominator = volume.astype(float).cumsum().replace(0, pd.NA)
-    return (typical * volume.astype(float)).cumsum() / denominator
-
